ae68e41abe3d87aef5cf7ae0bc04e6b3f4043ecb
IGAI Kiyofumi <igai.kiyofumi@office-craft.co.jp>
2025-09-03 13:30:45 +0900

    fix: versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "1.9.0"
+  required_version = "1.10.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.20.0"
+      version = "6.9.0"
     }
   }
 }
